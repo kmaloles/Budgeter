2419999796aaf4c53d751406d4bbfd68122d9178
kmaloles <kmaloles@ticketbiscuit.com>
2016-12-22 10:06:09 -0600

    Initial Commit

---
+++ budgeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  budgeter
+//
+//  Created by Kevin Maloles on 22/12/2016.
+//  Copyright © 2016 KAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
